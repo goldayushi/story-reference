0c30755ad2473d0e1674322e3efc3d4c2b9e1c1a
ayu <goldayushi@gmail.com>
2020-05-09 18:29:38 +0900

    Add: エピソード詳細画面、編集画面の追加

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,4 +1,6 @@
 class EpisodesController < ApplicationController
+  before_action :set_episode, only: [:show, :edit, :update, :destroy]
+  
   def index
     @episodes = Episode.all.order(chapter: :desc).page(params[:page]).per(10)
   end
@@ -23,13 +25,22 @@ class EpisodesController < ApplicationController
   end
 
   def update
+    if @episode.update(episode_params)
+      redirect_to "/episodes/#{@episode.id}"
+    else
+      render 'new'
+    end
   end
 
   def destroy
   end
 
   private
-  def episode_params
-    params.require(:episode).permit(:chapter, :title, :overview, :content, :set, :keyword, :keyitem, :division, :year)
-  end
+    def set_episode
+      @episode = Episode.find(params[:id])
+    end
+    
+    def episode_params
+      params.require(:episode).permit(:chapter, :title, :overview, :content, :set, :keyword, :keyitem, :division, :year)
+    end
 end
