6823cc2304f4d879b73630fa8f15b00a9a224f81
ayu <goldayushi@gmail.com>
2020-05-10 20:14:04 +0900

    Add: 編集、削除画面

--- app/controllers/glossaries_controller.rb
+++ app/controllers/glossaries_controller.rb
@@ -1,4 +1,6 @@
 class GlossariesController < ApplicationController
+  before_action :set_glossary, only: [:show, :edit, :update, :destroy]
+
   def index
     if params[:tag_name]
       @glossaries = Glossary.tagged_with("#{params[:tag_name]}").page(params[:page]).per(20)
@@ -28,12 +30,26 @@ class GlossariesController < ApplicationController
   end
 
   def update
+    if @glossary.update(glossary_params)
+      redirect_to "/glossaries/#{@glossary.id}"
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    if @glossary.destroy
+      redirect_to glossaries_path
+    else
+      render 'index'
+    end
   end
 
   private
+    def set_glossary
+      @glossary = Glossary.find(params[:id])
+    end
+    
     def glossary_params
       params.require(:glossary).permit(:title, :content, :chapter, :tag_list)
     end
--- app/models/glossary.rb
+++ app/models/glossary.rb
@@ -1,3 +1,5 @@
 class Glossary < ApplicationRecord
 	acts_as_taggable
+
+	validates :title, presence: true, length: { maximum: 100 }, uniqueness: true
 end
