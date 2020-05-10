7f829bb2d752b27ecc34113adb31d19377873ab0
ayu <goldayushi@gmail.com>
2020-05-10 14:40:56 +0900

    Add: タグ機能

--- app/controllers/glossaries_controller.rb
+++ app/controllers/glossaries_controller.rb
@@ -1,7 +1,11 @@
 class GlossariesController < ApplicationController
   def index
-    @q = Glossary.ransack(params[:q])
-    @glossaries = @q.result(distinct: true).order(:chapter).page(params[:page]).per(20)
+    if params[:tag_name]
+      @glossaries = Glossary.tagged_with("#{params[:tag_name]}").page(params[:page]).per(20)
+    else
+      @q = Glossary.ransack(params[:q])
+      @glossaries = @q.result(distinct: true).order(:chapter).page(params[:page]).per(20)
+    end
   end
 
   def new
@@ -31,6 +35,6 @@ class GlossariesController < ApplicationController
 
   private
     def glossary_params
-      params.require(:glossary).permit(:title, :content, :chapter)
+      params.require(:glossary).permit(:title, :content, :chapter, :tag_list)
     end
 end
--- app/models/glossary.rb
+++ app/models/glossary.rb
@@ -1,2 +1,3 @@
 class Glossary < ApplicationRecord
+	acts_as_taggable
 end
