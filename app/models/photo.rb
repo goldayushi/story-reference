dcb47a9db882fdf3dd6c16f9bd64e88bdb45db23
ayu <goldayushi@gmail.com>
2020-05-16 13:12:47 +0900

    Fix: 画像庫レイアウト修正

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,7 +2,5 @@ class Photo < ApplicationRecord
   acts_as_taggable
   
   mount_uploader :image, ImagesUploader
-  #skip_callback :commit, :after, :remove_image!
-  
-  #validates :image, presence: true
+  skip_callback :commit, :after, :remove_image!
 end
