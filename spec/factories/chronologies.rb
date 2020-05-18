8d14725bce5973c1944282158b636709d824a3ea
ayu <goldayushi@gmail.com>
2020-05-19 00:19:07 +0900

    Add: 用語集のリクエストテスト追加

--- spec/factories/chronologies.rb
+++ spec/factories/chronologies.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :chronology, class: Chronology do
     year { 500 }
-    japan_event { 'overview' }
-    korea_event { 'content' }
-    china_event { 'set' }
-    other_event { 'keyitem' }
+    japan_event { 'japan_event' }
+    korea_event { 'korea_event' }
+    china_event { 'china_event' }
+    other_event { 'other_event' }
   end
 end
