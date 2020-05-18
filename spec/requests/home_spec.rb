916c94c7fa623afa9c16c2fb5b77a07fba9c04bf
ayu <goldayushi@gmail.com>
2020-05-19 00:25:12 +0900

    Add: トップ画面リクエストテスト

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "home", type: :request do
+
+  describe "adminユーザ操作" do
+    let!(:login_user) { create(:adm_user) }
+
+    before do
+      post '/login', params: { session: { name: 'kyokai', password: 'kyokaipw' } }
+    end
+
+    context "トップ画面" do
+      it "GET /" do
+        get '/'
+        expect(response).to have_http_status(200)
+        expect(response.body).to include 'ユーザ管理'
+        expect(response.body).to include 'ログアウト'
+      end
+    end
+  end
+
+  describe "normalユーザ操作" do
+    let!(:login_user) { create(:normal_user) }
+
+    before do
+      post '/login', params: { session: { name: 'ohon', password: 'ohonpw' } }
+    end
+
+    context "トップ画面" do
+      it "GET /" do
+        get '/'
+        expect(response).to have_http_status(200)
+        expect(response.body).not_to include 'ユーザ管理'
+        expect(response.body).to include 'ログアウト'
+      end
+    end
+  end
+end
