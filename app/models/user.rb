6198f441f6f0850a8efb8e31f8269524c6e87a9c
ayu <goldayushi@gmail.com>
2020-05-19 00:38:13 +0900

    Fix: ユーザ名をユニークにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 	has_secure_password
 
-	validates :name, presence: true, on: :create
+	validates :name, presence: true, uniqueness: true, on: :create
 	validates :password, presence: true, on: :create
 end
